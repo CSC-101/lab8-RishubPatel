5046c11a8cfd1350e550ef7728d0e8b067921ac1
RishubPatel <rpate133@calpoly.edu>
2024-11-12 12:02:34 -0800

    Worked on task 2

--- file.py
+++ file.py
@@ -1,2 +1,26 @@
 def sums_from_file(file_name: str) -> None:
-    pass
+    try:
+        file = (open(file_name, "r"))
+    except:
+        print("Error")
+        exit()
+    for line in file:
+        if "," in line:
+            floats = line.split(",")
+        else:
+            floats = line.split()
+        if len(floats) != 2:
+            print("There aren't 2 items in this line.")
+        else:
+            try:
+                float_1 = float(floats[0])
+                float_2 = float(floats[1])
+                print(float_1 + float_2)
+            except IndexError:
+                print("There aren't 2 floats")
+            except ValueError:
+                print("Something besides a float")
+
+
+if __name__ == "__main__":
+    sums_from_file("floats.txt")
